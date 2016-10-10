18ea2b76d1330059a23e985f5df7916cd76a92c2
Malcom Gilbert <malcomgilbert@gmail.com>
2016-10-10 13:01:23 -0700

    make snapsvg work with webpack

--- src/shell.jsx
+++ src/shell.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import styles from './shell.styl'
+import Snap from 'snapsvg'
 
 class Shell extends React.Component {
 
 
   componentDidMount() {
+
+    var s = Snap("#svgLand");
+    // var s = Snap(400, 200);
+    var circle = s.circle(100, 100, 100);
+    var rect = s.rect(200, 0, 200, 200);
   }
 
   shouldComponentUpdate() {
@@ -14,7 +20,8 @@ class Shell extends React.Component {
     return <div className={styles.main}>
       <div className={styles.sidebar}>
       </div>
-      <div className={styles.content} id='svgLand'>
+      <div className={styles.content} >
+        <svg id='svgLand'></svg>
       </div>
     </div>
   }
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,11 @@ const config = env => ({
   module: {
     loaders: [
       {
+          // https://github.com/adobe-webplatform/Snap.svg/issues/341#issuecomment-143267637
+          test: require.resolve('snapsvg'),
+          loader: 'imports-loader?this=>window,fix=>module.exports=0'
+      },
+      {
         test: /\.jsx?$/,
         loader: ['babel-loader'],
         exclude,
